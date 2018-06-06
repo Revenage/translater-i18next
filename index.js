f620446165ab80e4499874498ca1916e6013a61f
Revenage <Revenage@users.noreply.github.com>
2018-06-06 19:02:31 +0300

    up

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 const googleTranslate = require('google-translate-api');
 var translate = require("./translates.json");
+var custom = require("./custom.json");
+
 var defaultLang = 'en'
 
 const languages = Object.keys(translate);
@@ -16,36 +18,57 @@ function getTranslate(str, lang, cb) {
 }
 
 function setTraslate(lang, wordsKey, translated, last) {
-    //console.log(lang, wordsKey, translated);
-
     translate[lang].translations[wordsKey] = translated;
-    console.log(translate, last);
-
-    if (last) {
-        setTimeout(() => {
-            write(translate);
-        }, 1000)
-    }
 }
 
 restLanguages.forEach(lang => {
     wordsKeys.forEach(wordsKey => {
         const text = translate[lang].translations[wordsKey];
-        if (!text) {
+        if (text == null) {
             const defaultText = translate[defaultLang].translations[wordsKey]
-            console.log(`Translate for text ${defaultText} in ${lang} is empty`)
+            console.log(`Translate for text ${defaultText} in ${lang} is empty [${wordsKey}]`)
             const last = restLanguages[restLanguages.length - 1] === lang && wordsKeys[wordsKeys.length - 1] === wordsKey;
+
+            if (custom[lang] && custom[lang].translations[wordsKey]) {
+                setTraslate(lang, wordsKey, custom[lang].translations[wordsKey], last);
+                return;
+            }
             getTranslate(defaultText, lang, translated => setTraslate(lang, wordsKey, translated, last));
         }
     })
 })
 
+
+setTimeout(() => {
+    write(translate);
+}, 1000)
+
+function ksort(src) {
+    const keys = Object.keys(src);
+    keys.sort();
+    return keys.reduce((target, key) => {
+        target[key] = src[key];
+        return target;
+    }, {});
+};
+
+function toOrder(obj) {
+    languages.forEach(lang => {
+        obj[lang].translations = ksort(obj[lang].translations);
+    })
+    return obj;
+}
+
 function write(trans) {
-    fs.writeFile("./object.json", JSON.stringify(trans, null, 4), (err) => {
+
+    // const data = JSON.stringify(toOrder(trans), (k, v) => ((v === undefined || v === '"undefined"') ? '""' : v), 4)
+
+    fs.writeFile("output.json", JSON.stringify(toOrder(trans), null, 4), (err) => {
         if (err) {
             console.error(err);
             return;
         };
         console.log("File has been created");
     });
+    // fs.writeFile("revision.json", data, err);
 }
